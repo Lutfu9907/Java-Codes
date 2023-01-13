25b0dcbb28d0b7a9a081da6e9df26292c4c1decf
LUTFU <93095154+Lutfu9907@users.noreply.github.com>
2023-01-13 15:09:34 +0300

    Update Ornek3.java

--- Homeworks/week9/Ornek3.java
+++ Homeworks/week9/Ornek3.java
@@ -1,4 +1,4 @@
-//(2k+2)/(2k+1)!
+
 package week9;
 
 import java.util.Scanner;
@@ -7,17 +7,10 @@ public class Ornek3 {
 
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("n Sayısını Giriniz: ");
-        int n = input.nextInt();
+        int n = scanner.nextInt();
         double toplam = 0;
-       /* for (int i = 2; i <= (n-2)/2; i+=2) {
-            int fakt = 1;
-            for (int j = i; j <= i-1; j++) {
-                fakt *= j;
-            }
-            toplam += i/fakt; 
-        }*/
        
         for (int i = 0; i <= n; i++) {
             int temp=2*i+1;
